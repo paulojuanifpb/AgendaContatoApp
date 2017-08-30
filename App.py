0e6d93bf3529a9519816990e58865c5af942951b
paulojuanifpb <paulojuan12.h@gmail.com>
2017-08-30 10:23:43 -0300

    Update App.py

--- App.py
+++ App.py
@@ -4,28 +4,32 @@ from model.Contato import Contato
 from model.Telefone import Telefone
 from datetime import datetime
 import json
+import pickle
+
 
 def main():
-    e1 = Agenda("paulo"," "," "," ",00,00,00)
+    
+    #cadastro do proprietário
+    print("INFORME OS DADOS QUE SERÃO PEDIDOS PARA SER CADASTRADO COMO PROPRIETARIO:\n ")
+    e1 = Agenda(proprietario = input('DIGITE O SEU NOME:\n'),nascimento = input("DIDITE A SUA DADTA DE NASCIMENTO:\n"),email= input("DIGITE O SEU EMAIL:\n"))
+    dicp = {'Nome': e1.proprietario , 'Data de Nascimento': e1.nascimento, 'Email': e1.email}
+    arquivo = open("proprietario.json",'wb')
+    pickle.dump(dicp,arquivo)
+    arquivo.close()
+    arquivo = open("proprietario.json","rb")
+    dicp = pickle.load(arquivo)
+    arquivo.close()
     menu(e1)
-
+    
 def menu(e1):
     while True :
-        try:
+
             print("Menu : \n 1 - Incluir contato\n 2 - Listar contatos\n 3 - Remover contato\n 4 - Buscar contato\n 5 - Quantidade de contatos \n 6 - Sair")
             resp = int(input("Escolha uma das opções do menu :\n"))
 
             if (resp == 1):
-                nome = str(input("Informe o seu nome:"))
-                nascimento = str(input("Informe a sua data de nascimento:"))
-                email = str(input("Informe o seu email:"))
-                numero = int(input("Informe o seu numero:"))
-                ddd = int(input("Informe o seu ddd:"))
-                codigoPais = int(input("Informe o código do seu País:"))
-                criacao = datetime
-                pessoa = Pessoa(nome, nascimento, email)
-                telefone = Telefone(numero, ddd, codigoPais)
-                contato = Contato(pessoa, criacao, telefone)
+               e1.incluirContato()
+
 
             elif (resp == 2):
                 e1.listarContatos()
@@ -36,8 +40,8 @@ def menu(e1):
             elif (resp == 5):
                 e1.contarContatos()
             break
-        except:
-            print("Ouve algum erro... Please, tente novamente!")
-
+            
+ 
 if __name__ == "__main__":
     main()
+          
